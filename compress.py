40e971ae2ce9c68e9f65a4c4d14614d9c91c6fdf
Jacky Liang <jackyliang@cmu.edu>
2022-05-15 22:33:49 -0400

    init commit

---
+++ compress.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import subprocess
+import ray
+
+
+@ray.remote(num_cpus=1)
+def compress(input_filepath, output_filepath, target_size_mb):
+    cmd = ['bash', 'compress.sh', str(input_filepath), str(output_filepath), str(target_size_mb)]
+    print('running ', ' '.join(cmd))
+    p = subprocess.Popen(
+            cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    p.communicate()
+    return p.returncode
+
+
+if __name__ == '__main__':
+    input_path = Path('G:/storage/research/corl2022/videos/real/raw')
+    output_path = Path('videos_output')
+    output_size = 50
+
+    ray.init()
+    ray_ids = []
+    for name in os.listdir(input_path):
+        input_name = input_path / name
+        name_no_ext = name.split('.')[0]
+        output_name = output_path / (name_no_ext + '_sm.mp4')
+
+        ray_ids.append(compress.remote(input_name, output_name, output_size))
+
+    return_codes = ray.get(ray_ids)
+        
